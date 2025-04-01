87a092a017979c14d2e1d5c28c12455de8d0a29e
exoad <jackmeng0814@gmail.com>
2025-03-31 22:09:37 -0400

    reedit

--- deploy/flutter_bootstrap.js
+++ deploy/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1107165103"
+    serviceWorkerVersion: "3965919994"
   }
 });
